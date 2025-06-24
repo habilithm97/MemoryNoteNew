7030d139170ac52b1e43509ccc0ab35e5b98c6d2
habilithm97 <habinkim97@gmail.com>
2025-06-24 16:57:45 +0900

    메모 Room DAO

--- app/src/main/java/com/example/memorynotenew/room/memo/Memo.kt
+++ app/src/main/java/com/example/memorynotenew/room/memo/Memo.kt
@@ -3,6 +3,7 @@ package com.example.memorynotenew.room.memo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+// DB 테이블에 매핑되는 데이터 클래스
 @Entity(tableName = "memos")
 data class Memo(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
---
+++ app/src/main/java/com/example/memorynotenew/room/memo/MemoDao.kt
@@ -0,0 +1,37 @@
+package com.example.memorynotenew.room.memo
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+// DB 접근 메서드 정의
+@Dao
+interface MemoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMemo(memo: Memo)
+
+    @Update
+    suspend fun updateMemo(memo: Memo)
+
+    @Delete
+    suspend fun deleteMemo(memo: Memo)
+
+    @Query("select * from memos order by date")
+    fun getAllMemos(): Flow<List<Memo>>
+}
+
+/*
+* suspend
+ -오래 걸리는 작업 비동기 처리 (Room DB, 네트워크)
+ -UI 스레드 차단 없이 중단/재개
+
+* Flow
+ -비동기 데이터 스트림 처리 (코루틴 기반)
+ -순차적/지속적 데이터 방출 (emit)
+ -UI 생명주기와 독립적 (LiveData와의 차이점)
+ -구독 방식으로 실시간 데이터 수신 (collect)
+ */
