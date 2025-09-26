3553a6ee5b662f02ba1e4bea9e73a9f8713059e0
habilithm97 <habinkim97@gmail.com>
2025-09-26 12:57:34 +0900

    Repository에 휴지통 Room DAO 추가

--- app/src/main/java/com/example/memorynotenew/repository/MemoRepository.kt
+++ app/src/main/java/com/example/memorynotenew/repository/MemoRepository.kt
@@ -1,23 +1,47 @@
 package com.example.memorynotenew.repository
 
 import com.example.memorynotenew.room.dao.MemoDao
+import com.example.memorynotenew.room.dao.TrashDao
 import com.example.memorynotenew.room.entity.Memo
+import com.example.memorynotenew.room.entity.Trash
 import kotlinx.coroutines.flow.Flow
 
 /* 데이터 저장소와 ViewModel 사이의 다리
  -ViewModel은 데이터 출처를 몰라도 됨 (데이터 소스 추상화)
  */
-class MemoRepository(private val memoDao: MemoDao) {
+class MemoRepository(
+    private val memoDao: MemoDao,
+    private val trashDao: TrashDao
+) {
     suspend fun insertMemo(memo: Memo) {
         memoDao.insertMemo(memo)
     }
+
     suspend fun updateMemo(memo: Memo) {
         memoDao.updateMemo(memo)
     }
+
     suspend fun deleteMemo(memo: Memo) {
         memoDao.deleteMemo(memo)
     }
+
     fun getAllMemos(): Flow<List<Memo>> {
         return memoDao.getAllMemos()
+    }
+
+    suspend fun insertTrash(trash: Trash) {
+        trashDao.insertTrash(trash)
+    }
+
+    suspend fun deleteTrash(trash: Trash) {
+        trashDao.deleteTrash(trash)
+    }
+
+    fun getAllTrash(): Flow<List<Trash>> {
+        return trashDao.getAllTrash()
+    }
+
+    suspend fun deleteOldTrash(limit: Long) {
+        trashDao.deleteOldTrash(limit)
     }
 }
