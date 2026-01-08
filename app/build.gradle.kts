4c28e165a20a5812ffc020cf917b3b3c03f6bbf8
habilithm97 <habinkim97@gmail.com>
2026-01-08 23:45:57 +0900

    라이브러리 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.memorynotenew"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.habilithm.memorynote"
